b7715ed1808600507369a798d6f8baa44b2f39ae
MadPsy <madpsy@users.noreply.github.com>
2025-11-21 15:48:28 +0000

    cw

--- static/app.js
+++ static/app.js
@@ -1115,7 +1115,7 @@ async function fetchSiteDescription() {
                         const leafletCSS = document.createElement('link');
                         leafletCSS.id = 'leaflet-css';
                         leafletCSS.rel = 'stylesheet';
-                        leafletCSS.href = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css';
+                        leafletCSS.href = 'leaflet.css';
                         leafletCSS.integrity = 'sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=';
                         leafletCSS.crossOrigin = '';
                         document.head.appendChild(leafletCSS);
@@ -1123,7 +1123,7 @@ async function fetchSiteDescription() {
 
                     if (!window.L) {
                         const leafletJS = document.createElement('script');
-                        leafletJS.src = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
+                        leafletJS.src = 'leaflet.js';
                         leafletJS.integrity = 'sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=';
                         leafletJS.crossOrigin = '';
                         document.head.appendChild(leafletJS);

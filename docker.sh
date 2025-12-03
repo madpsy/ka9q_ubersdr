230cb836df64d536e673b2a1440503a04462475f
MadPsy <madpsy@users.noreply.github.com>
2025-12-03 17:13:40 +0000

    gui

--- docker.sh
+++ docker.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-docker build -t madpsy/ka9q_ubersdr:0.8.1 -f docker/Dockerfile .
-docker push madpsy/ka9q_ubersdr:0.8.1
+VERSION=0.8.1
+IMAGE=madpsy/ka9q_ubersdr
+
+# Build image with version tag
+docker build -t $IMAGE:$VERSION -f docker/Dockerfile .
+
+# Tag version as latest
+docker tag $IMAGE:$VERSION $IMAGE:latest
+
+# Push both tags
+docker push $IMAGE:$VERSION
+docker push $IMAGE:latest

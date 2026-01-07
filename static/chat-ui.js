7913996ccc0ff7ab2c152e6888dbd3628b1c2ac9
MadPsy <madpsy@users.noreply.github.com>
2026-01-07 12:53:50 +0000

    Bandwidth

--- static/chat-ui.js
+++ static/chat-ui.js
@@ -320,7 +320,7 @@ class ChatUI {
             }
 
             .chat-panel.expanded {
-                width: 690px;
+                width: 540px;
             }
             
             .chat-header {
@@ -360,7 +360,7 @@ class ChatUI {
             }
             
             .chat-content {
-                width: 650px;
+                width: 500px;
                 height: 500px;
                 background: rgba(40, 40, 40, 0.7);
                 border: 1px solid rgba(100, 100, 100, 0.6);

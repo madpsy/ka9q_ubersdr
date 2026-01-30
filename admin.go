bb89c12785e5c6390ccc349c249812524f433243
MadPsy <madpsy@users.noreply.github.com>
2026-01-30 19:13:20 +0000

    0.1.36

--- admin.go
+++ admin.go
@@ -3672,7 +3672,7 @@ func (ah *AdminHandler) HandleSystemStats(w http.ResponseWriter, r *http.Request
 	// Get IP address information via GoTTY if SSH proxy is enabled
 	gottyClient := NewGoTTYClient(&ah.config.SSHProxy)
 	if gottyClient != nil {
-		if resp, err := gottyClient.ExecCommand("ip address show", 10); err == nil && resp.ExitCode == 0 {
+		if resp, err := gottyClient.ExecCommand("ip address show", 2); err == nil && resp.ExitCode == 0 {
 			stats["ip_addresses"] = resp.Stdout
 		}
 	}

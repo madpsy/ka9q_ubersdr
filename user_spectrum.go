115ad98998967b60a3f51a67dba9840de07d971f
MadPsy <madpsy@users.noreply.github.com>
2026-02-19 22:46:06 +0000

    0.1.40

--- user_spectrum.go
+++ user_spectrum.go
@@ -121,6 +121,16 @@ func (usm *UserSpectrumManager) setupStatusListener() error {
 
 	udpConn := conn.(*net.UDPConn)
 
+	// Increase UDP receive buffer to handle bursts from many users
+	// Default is typically 208KB, increase to 8MB to prevent packet loss
+	// With 40+ users, radiod may send all responses in a tight burst
+	const recvBufferSize = 8 * 1024 * 1024 // 8MB
+	if err := udpConn.SetReadBuffer(recvBufferSize); err != nil {
+		log.Printf("Warning: failed to set UDP receive buffer size to %d bytes: %v", recvBufferSize, err)
+	} else {
+		log.Printf("UDP receive buffer set to %d MB for spectrum data", recvBufferSize/(1024*1024))
+	}
+
 	// Join multicast group
 	if usm.statusAddr.IP.IsMulticast() {
 		iface := usm.radiod.GetInterface()

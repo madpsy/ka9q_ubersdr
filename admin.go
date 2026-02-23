e929cbace0141b5621ab7eb0e9b9c6d1142e891b
MadPsy <madpsy@users.noreply.github.com>
2026-02-23 08:15:38 +0000

    0.1.40

--- admin.go
+++ admin.go
@@ -4594,7 +4594,7 @@ func (ah *AdminHandler) HandleSessionActivityChartData(w http.ResponseWriter, r 
 
 	// Determine bucket size based on time range
 	duration := endTime.Sub(startTime)
-	bucketMinutes := 15 // Default: 15 minutes for day view
+	bucketMinutes := 1 // Default: 1 minute for day view (24 hours)
 	if duration > 7*24*time.Hour {
 		bucketMinutes = 360 // 6 hours for month view
 	} else if duration > 24*time.Hour {

312936919bbd57dac8b2a66730eb947bd1ef896d
MadPsy <madpsy@users.noreply.github.com>
2026-06-08 11:36:17 +0100

    0.1.52

--- version.go
+++ version.go
@@ -2,4 +2,4 @@ package main
 
 // Version is the current version of ka9q_ubersdr
 // Ensure Tunnel has latest version before pushing container (./sync_tunnel.sh <version>)
-const Version = "0.1.51"
+const Version = "0.1.52"
